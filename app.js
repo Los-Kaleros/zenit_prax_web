4b61d4460cb93fb6e7805382913d02f15cb2911a
Kristian Válek <valekkristian2@gmail.com>
2023-01-14 22:01:46 +0100

    added js, secrets, little changes

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 function showSecret() {
-    document.getElementById('image').style.opacity = 1;
+    document.getElementById("image").style.display = "block";
+    document.getElementById("image").style.opacity = 1;
+    setTimeout(function hideSecret(){
+        document.getElementById("image").style.display = "none";
+    }, 5000); 
+    
 }
 
-function hideSecret() {
-    document.getElementById('image').style.opacity = 0;
-}
 
 function ricardoShow() {
     document.getElementById("ricardos").style.opacity = 1;
+    document.getElementById("ricardos").style.width = "50vw";
     new Audio('images/ricardo.mp3').play();
+    document.getElementById("ricardos").style.zIndex=99;
+    alert("Ricardo has come for you!!!")
     setTimeout(function ricardoHide(){
         document.getElementById("ricardos").style.opacity = 0;
-    }, 11000);
+        document.getElementById("ricardos").style.width = "0.1vw";
+    }, 12000);
 }
 
-
-
+function alertFind(){
+    alert("Sorko žiadne domény");
+}
